89be96db679b4c39632b6f24907e17b1762e9562
adrielaryssa <adrilaryssa@gmail.com>
2021-08-12 21:46:17 -0300

    Colocando em LocalStorage

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 'use strict';
 
+let banco = [];
+
+const getBanco = () => JSON.parse(localStorage.getItem ('todoList')) ?? [];
+const setBanco = (banco) => localStorage.setItem ('todoList', JSON.stringify(banco));
+
 const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label');
     item.classList.add('todo__item');
--- banco.sql
+++
@@ -1,4 +0,0 @@
-let banco = [];
-
-const getBanco = () => JSON.parse(localStorage.getItem ('Tarefas')) ?? [];
-const setBanco = (banco) => localStorage.setItem ('Tarefa', JSON.stringify(banco));
